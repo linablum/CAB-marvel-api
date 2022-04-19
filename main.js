6d0b4d9f494fa0816dc7ade96f892b68f89b6814
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-04-19 13:18:53 +0200

    save response in js

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-http://gateway.marvel.com/v1/public/characters?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e
+console.log(data);
+
+// http://gateway.marvel.com/v1/public/characters?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e

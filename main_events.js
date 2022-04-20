2869669d4ee17678f339821d26acd1e7fbc4876b
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-04-20 22:39:29 +0200

    function toggle checkbox

--- main_events.js
+++ main_events.js
@@ -1,27 +1,33 @@
 console.log(data2);
 
-//x = Object.keys(data.data.results).length
+//events = [15, 2, 30, 0, 55];
 
-/* for (let i = 0; i < 50; i++) {
-  let list = document.createElement("li");
-  list.innerHTML = data.data.results[i].name;
-  document.getElementById("text").appendChild(list);
-} */
+function eventChar(x, y) {
+  for (
+    let i = 0;
+    i < Object.keys(data2.data.results[x].characters.items).length;
+    i++
+  ) {
+    let list = document.createElement("li");
+    list.innerHTML = data2.data.results[x].characters.items[i].name;
+    document.getElementById("api-data").appendChild(list);
+    list.style.display = "none";
 
-//civil war: 15
-//age of: 2
-//infinity: 30
-//gauntlet: 31
-//secret: 55
-
-for (
-  let i = 0;
-  i < Object.keys(data2.data.results[2].characters.items).length;
-  i++
-) {
-  let list = document.createElement("li");
-  list.innerHTML = data2.data.results[2].characters.items[i].name;
-  document.getElementById("text").appendChild(list);
+    let checkbox = document.getElementById(y);
+    checkbox.addEventListener("change", function displayChar() {
+      if (list.style.display === "none") {
+        list.style.display = "block";
+      } else {
+        list.style.display = "none";
+      }
+    });
+  }
 }
+
+eventChar(15, "btncheck1");
+eventChar(2, "btncheck2");
+eventChar(30, "btncheck3");
+eventChar(0, "btncheck4");
+eventChar(55, "btncheck5");
 
 // http://gateway.marvel.com/v1/public/characters?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e

0d9d74836b5ad165c8962647daf8a23e4b670457
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-04-21 13:41:32 +0200

    js folder

--- main.js
+++
@@ -1,9 +0,0 @@
-console.log(data);
-
-//x = Object.keys(data.data.results).length
-
-/* for (let i = 0; i < 50; i++) {
-  let list = document.createElement("li");
-  list.innerHTML = data.data.results[i].name;
-  document.getElementById("text").appendChild(list);
-} */

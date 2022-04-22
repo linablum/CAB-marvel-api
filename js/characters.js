19f45a64bbcd34b20b26257fc17073300bfa0127
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-04-22 15:59:04 +0200

    add character cards

--- js/characters.js
+++ js/characters.js
@@ -1,41 +1,61 @@
-let divContainer = document.getElementById("container");
-
-for (i = 0; i < 10; i++) {
-  let divCard = document.createElement("div");
-  divCard.classList.add("card");
-  divCard.classList.add("transparent");
-  divCard.classList.add("mb-3");
-  let divRow = document.createElement("div");
-  divRow.classList.add("row g-0");
-  let divCol = document.createElement("div");
-  divCol.classList.add("col-md-4");
-  let img = document.createElement("img");
-  img.classList.add("img-fluid rounded-start");
-  img.setAttribute("src", data.data.results[i].thumbnail);
-  let;
-}
-
-const searchBar = document.getElementById("search");
-let marvelCharacter = [];
+let url =
+  "http://gateway.marvel.com/v1/public/characters?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e&limit=100";
 
 async function loadChar() {
-  const res = await fetch(
-    "http://gateway.marvel.com/v1/public/characters?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e&limit=100"
-  );
+  const res = await fetch(url);
   const marvelCharacter = await res.json();
-  showChar(marvelCharacter);
+  showCard(marvelCharacter);
+  console.log(marvelCharacter);
 }
 
-function showChar(data) {
+/* function showChar(data) {
   for (let i = 0; i < 30; i++) {
     let list = document.createElement("li");
     list.innerHTML = data.data.results[i].name;
     document.getElementById("test").appendChild(list);
   }
-}
+} */
 
 loadChar();
 
+function showCard(data) {
+  let divContainer = document.getElementById("container");
+  for (i = 0; i < 10; i++) {
+    let divCard = document.createElement("div");
+    divCard.classList.add("card", "bg-transparent", "mb-3");
+    divCard.setAttribute("style", "max-width: 540px;");
+    let divRow = document.createElement("div");
+    divRow.classList.add("row", "g-0");
+    let divColImg = document.createElement("div");
+    divColImg.classList.add("col-md-4");
+    let img = document.createElement("img");
+    img.classList.add("rounded-start", "img-fluid");
+    img.setAttribute("src", data.data.results[i].thumbnail.path + ".jpg");
+    img.setAttribute("style", "width: 6rem");
+    let divColText = document.createElement("div");
+    divColText.classList.add("col-md-8");
+    let divCardBody = document.createElement("div");
+    divCardBody.classList.add("card-body");
+    let hCardTitle = document.createElement("h5");
+    hCardTitle.classList.add("card-title");
+    hCardTitle.innerHTML = data.data.results[i].name;
+    let pCardText = document.createElement("p");
+    pCardText.classList.add("card-text");
+    pCardText.innerHTML = data.data.results[i].description;
+
+    divContainer.appendChild(divCard);
+    divCard.appendChild(divRow);
+    divRow.appendChild(divColImg);
+    divRow.appendChild(divColText);
+    divColImg.appendChild(img);
+    divColText.appendChild(divCardBody);
+    divCardBody.appendChild(hCardTitle);
+    divCardBody.appendChild(pCardText);
+  }
+}
+
+const searchBar = document.getElementById("search");
+let marvelCharacter = [];
 searchBar.addEventListener("keyup", (e) => {
   const searchString = e.target.value;
   console.log(searchString);
--- js/events.js
+++ js/events.js
@@ -1,9 +1,8 @@
-//events = [15, 2, 30, 0, 55];
+let url =
+  "https://gateway.marvel.com:443/v1/public/events?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e&limit=100";
 
 async function loadChars() {
-  const res = await fetch(
-    "https://gateway.marvel.com:443/v1/public/events?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e&limit=100"
-  );
+  const res = await fetch(url);
   const apiData = await res.json();
   console.log(apiData);
   const eventArray = [15, 2, 30, 0, 55];

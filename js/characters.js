b5502888787b1c2ee5a54302a3f918110902d344
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-04-23 14:59:08 +0200

    add md5 script

--- js/characters.js
+++ js/characters.js
@@ -1,6 +1,12 @@
 let url =
   "http://gateway.marvel.com/v1/public/characters?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e&limit=100";
 
+/* function getHash() {
+  const timestamp = new Date().getTime();
+  const md5Hash = md5(timestamp + ApiKey.PRIVATE + ApiKey.PUBLIC);
+  return `ts=${timestamp}&apikey=${ApiKey.PUBLIC}&hash=${md5Hash}`;
+} */
+
 /* fetch(
   "https://gateway.marvel.com:443/v1/public/events?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e&limit=100"
 )
@@ -26,18 +32,24 @@ loadChar();
 
 function showCard(data) {
   let divContainer = document.getElementById("container");
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < 15; i++) {
     let divCard = document.createElement("div");
-    divCard.classList.add("card", "bg-transparent", "mb-3");
-    divCard.setAttribute("style", "max-width: 740px;");
+    divCard.classList.add("card", "bg-transparent", "mb-3", "frosted");
+    divCard.setAttribute("style", "max-width:640px");
     let divRow = document.createElement("div");
     divRow.classList.add("row", "g-0");
     let divColImg = document.createElement("div");
     divColImg.classList.add("col-md-4");
+    divColImg.setAttribute("style", "width:8rem");
     let img = document.createElement("img");
     img.classList.add("rounded-start", "img-fluid");
-    img.setAttribute("src", data.data.results[i].thumbnail.path + ".jpg");
-    img.setAttribute("style", "width: 6rem");
+    img.setAttribute(
+      "src",
+      data.data.results[i].thumbnail.path +
+        "." +
+        data.data.results[i].thumbnail.extension
+    );
+    img.setAttribute("style", "width: 7rem");
     let divColText = document.createElement("div");
     divColText.classList.add("col-md-8");
     let divCardBody = document.createElement("div");
@@ -47,16 +59,25 @@ function showCard(data) {
     hCardTitle.innerHTML = data.data.results[i].name;
     let pCardText = document.createElement("p");
     pCardText.classList.add("card-text");
+    let pCardSmall = document.createElement("p");
+    pCardSmall.classList.add("card-text");
     pCardText.innerHTML = data.data.results[i].description;
+    let small = document.createElement("small");
+    small.classList.add("text-muted");
+    small.innerHTML =
+      "Number of comics: " + data.data.results[i].comics.available;
 
     divContainer.append(divCard);
     divCard.append(divRow);
     divRow.append(divColImg, divColText);
     divColImg.append(img);
     divColText.append(divCardBody);
-    divCardBody.append(hCardTitle, pCardText);
+    divCardBody.append(hCardTitle, pCardText, pCardSmall);
+    pCardSmall.append(small);
   }
 }
+
+//<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
 const searchBar = document.getElementById("search");
 let marvelCharacter = [];
--- js/events.js
+++ js/events.js
@@ -1,14 +1,14 @@
 let url =
-  "https://gateway.marvel.com:443/v1/public/events?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e&limit=100";
+  "http://gateway.marvel.com/v1/public/characters?apikey=b8e5c1e286dbfe1ff75fb79aa0ac5957&ts=1&hash=31b62279aa5d465c1ff1206582cea96e&limit=100";
 
 async function loadChars() {
   const res = await fetch(url);
   const apiData = await res.json();
   console.log(apiData);
-  const eventArray = [15, 2, 30, 0, 55];
-  const events = eventArray.map((num, i) =>
-    eventChar(num, `btncheck${i + 1}`, apiData)
-  );
+  //  const eventArray = [15, 2, 30, 0, 55];
+  //  const events = eventArray.map((num, i) =>
+  //    eventChar(num, `btncheck${i + 1}`, apiData)
+  //  );
 }
 
 loadChars();
